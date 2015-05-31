5f493eb5a4d3bf5b634f255512fb9a13525f50cb
ssajewski <sarahsajewski@gmail.com>
2015-05-31 13:16:11 -0500

    Update predictions.R
    my latest predict modeling code. I think the code is pretty much done. I'm waiting to see if we get more memory space to interpret. If we get more space I'll run the models on all the data and interpret. If we don't get more space I'll run on the sample. Either way, I'll start interpreting things on thursday.

--- predictions.R
+++ predictions.R
@@ -11,7 +11,6 @@
 #{4}PREDICTION
 
 #{1}READ IN DATA
-#Note: This hasn't been adjusted to work with the github repo. 
 setwd("/Users/sarahsajewski/Dropbox/Harris/BigData2/Final Project/Openpayments data/")
 library(data.table)
 library(gamlr)
@@ -20,12 +19,13 @@ openpay <- as.data.frame(openpay)
 set.seed(06132015)
 openpay <- openpay[sample(1:nrow(openpay), 1e6, replace=FALSE),]
 source("OP.Factors.R")
-#source("naref.R")
+source("deviance.R")
 
 openpayfact <- sparse.model.matrix(~ charity+dispute+formpay+manu+mcountry+mstate+natpay
                                    +pown+proindic+pspec+ptype+rcountry+rpost+rprovince+rstate
                                    +rtype+rzip+suff+tcountry+thirdparty+thirdpartycover+tstate
                                   )
+openpay <- NULL
 #For some reason fname messes this up and reduces the nrow by 6. 
 
 #{2}LPAYAM MODELS
@@ -63,57 +63,83 @@ plot(paymodphys, main="paymodphys")
 dev.copy(png,'paymodphys.png')
 dev.off()
 
-pmcoef <- coef(paymod)[-1,]
-pmcoef.sig <- pmcoef[pmcoef!=0]
-pmcoef.pos <- pmcoef[pmcoef>0]
-pmcoef.neg <- pmcoef[pmcoef<0]
+pmcoef <- coef(paymod)
+pmcoef2 <-coef(paymod)[-1,]
+pmcoef.sig <- pmcoef2[pmcoef2!=0]
+pmcoef.pos <- pmcoef2[pmcoef2>0]
+pmcoef.neg <- pmcoef2[pmcoef2<0]
 names(pmcoef.sig)
 names(pmcoef.pos)
 names(pmcoef.neg)
+length(pmcoef.sig)
 pmcoef.pos[order(pmcoef.pos,decreasing=TRUE)[1:10]]
 pmcoef.neg[order(pmcoef.neg)[1:10]]
 
 #get coefficents for variables. 
-# coef.charity <- pmcoef[((as.numeric(grepl("charity*", rownames(pmcoef))))==1),]
-# coef.formpay <- pmcoef[((as.numeric(grepl("formpay*", rownames(pmcoef))))==1),]
-# coef.natpay <-  pmcoef[((as.numeric(grepl("natpay*", rownames(pmcoef))))==1),]
-# coef.numpay <- pmcoef[((as.numeric(grepl("numpay*", rownames(pmcoef))))==1),]
-# coef.manu <- pmcoef[((as.numeric(grepl("manu*", rownames(pmcoef))))==1),]
-# coef.mcountry <- pmcoef[((as.numeric(grepl("mcountry*", rownames(pmcoef))))==1),]
-# coef.pown <- pmcoef[((as.numeric(grepl("pown*", rownames(pmcoef))))==1),]
-# coef.proindic <- pmcoef[((as.numeric(grepl("proindic*", rownames(pmcoef))))==1),]
-# coef.pspec <- pmcoef[((as.numeric(grepl("pspec*", rownames(pmcoef))))==1),]
-# coef.rstate <- pmcoef[((as.numeric(grepl("rstate*", rownames(pmcoef))))==1),]
-# coef.rtype <- pmcoef[((as.numeric(grepl("rtype*", rownames(pmcoef))))==1),]
-# coef.rcountry <- pmcoef[((as.numeric(grepl("rcounty*", rownames(pmcoef))))==1),]
-# coef.rzip <- pmcoef[((as.numeric(grepl("rzip*", rownames(pmcoef))))==1),]
-# coef.thirdparty <- pmcoef[((as.numeric(grepl("thirdparty*", rownames(pmcoef))))==1),]
-# coef.suff <- pmcoef[((as.numeric(grepl("suff*", rownames(pmcoef))))==1),]
+coef.charity <- pmcoef[((as.numeric(grepl("charity*", rownames(pmcoef))))==1),]
+coef.formpay <- pmcoef[((as.numeric(grepl("formpay*", rownames(pmcoef))))==1),]
+coef.natpay <-  pmcoef[((as.numeric(grepl("natpay*", rownames(pmcoef))))==1),]
+coef.numpay <- pmcoef[((as.numeric(grepl("numpay*", rownames(pmcoef))))==1),]
+coef.manu <- pmcoef[((as.numeric(grepl("manu*", rownames(pmcoef))))==1),]
+coef.mcountry <- pmcoef[((as.numeric(grepl("mcountry*", rownames(pmcoef))))==1),]
+coef.pown <- pmcoef[((as.numeric(grepl("pown*", rownames(pmcoef))))==1),]
+coef.proindic <- pmcoef[((as.numeric(grepl("proindic*", rownames(pmcoef))))==1),]
+coef.pspec <- pmcoef[((as.numeric(grepl("pspec*", rownames(pmcoef))))==1),]
+coef.rstate <- pmcoef[((as.numeric(grepl("rstate*", rownames(pmcoef))))==1),]
+coef.rtype <- pmcoef[((as.numeric(grepl("rtype*", rownames(pmcoef))))==1),]
+coef.rcountry <- pmcoef[((as.numeric(grepl("rcounty*", rownames(pmcoef))))==1),]
+coef.rzip <- pmcoef[((as.numeric(grepl("rzip*", rownames(pmcoef))))==1),]
+coef.thirdparty <- pmcoef[((as.numeric(grepl("thirdparty*", rownames(pmcoef))))==1),]
+coef.suff <- pmcoef[((as.numeric(grepl("suff*", rownames(pmcoef))))==1),]
+
+
+#look into some coefs
+coef.pspec.sig <- coef.pspec[coef.pspec!=0]
+coef.pspec.pos <- coef.pspec[coef.pspec>0]
+coef.pspec.neg <- coef.pspec[coef.pspec<0]
+names(coef.pspec.sig)
+names(coef.pspec.pos)
+names(coef.pspec.neg)
+coef.pspec.pos[order(coef.pspec.pos,decreasing=TRUE)[1:10]]
+coef.pspec.neg[order(coef.pspec.neg)[1:10]]
 
 #sample
 #openpay <- openpay[sample(1:nrow(openpay), 1e5, replace=FALSE),]
 #source("OP.Factors.R")
 
 #{3}ONE PERCENT MODEL
-# oneper <- factor(payam >=quantile(payam, c(.99)))
+oneper <- factor(payam >=quantile(payam, c(.99)))
 # summary(oneper)
 # 
 # #limited the model to get it to run.
-# #General One Per Model
-# paymodoneper <- gamlr(openpayfact, oneper, family="binomial")
-# plot(paymodoneper, main="paymodoneper")
-# dev.copy(png,'oneper.png')
-# dev.off()
-# paymodoneperd <-paymodoneper$deviance[which.min(AICc(paymodoneper))]
+##PLAY AROUND WITH THIS. Maybe just 10,000? 
+oneperm<-sparse.model.matrix(~natpay+formpay+pspec+ptype+rstate)
+paymodoneper <- gamlr(oneperm, oneper, family="binomial")
+plot(paymodoneper, main="paymodoneper")
+dev.copy(png,'oneper.png')
+dev.off()
+paymodoneperd <-paymodoneper$deviance[which.min(AICc(paymodoneper))]
+
+opcoef <- coef(paymodoneper)[-1,]
+opcoef.sig <- opcoef[opcoef!=0]
+opcoef.pos <- opcoef[opcoef>0]
+opcoef.neg <- opcoef[opcoef<0]
+names(opcoef.sig)
+names(opcoef.pos)
+names(opcoef.neg)
+length(opcoef.sig)
+opcoef.pos[order(opcoef.pos,decreasing=TRUE)[1:10]]
+opcoef.neg[order(opcoef.neg)[1:10]]
+
 
 #{4}PREDICTION 
-# oneperpred <- predict(paymodoneper,openpayoneper)
-# plot(oneper,oneperpred)
-# dev.copy(png,'oneperpred.png')
-# dev.off()
 
 paymodspecpred <-predict(paymodspec, openpayform)
-plot(lpayam, paymodspecpred)
+paymodspecresid <- lpayam-paymodspecpred
+plot(lpayam,paymodspecresid)
+dev.copy(png,'paymodspecresid.png')
+dev.off()
+plot(lpayam,paymodspecpred)
 dev.copy(png,'paymodspecpred.png')
 dev.off()
 
@@ -123,15 +149,11 @@ dev.copy(png,'paymodlocpred.png')
 dev.off()
 
 paymodpred <- predict(paymod, openpayfact)
+paymodresid <- lpayam-paymodpred
+plot(lpayam,paymodresid)
+dev.copy(png,'paymodresid.png')
+dev.off()
 plot(lpayam,paymodpred)
 dev.copy(png,'paymodpred.png')
 dev.off()
-
-#{5}COMPONENT ANALYSIS
-#work in progress
-openpaypr <- sparse.model.matrix(~ formpay+natpay
-                                  +ptype)
-
-oppc <- prcomp(openpaypr, scale=TRUE)
-plot(oppc)
-
+R2(lpayam, paymodpred, family="gaussian")
